868cda08a15fee51db1d7e7599c59e202d1e7a04
TDNasty21 <32580253+TDNasty21@users.noreply.github.com>
2017-11-29 22:48:18 -0500

    Finished m9

--- src/m9_summing_again.py
+++ src/m9_summing_again.py
@@ -4,8 +4,8 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Tyler Nottingham.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -17,7 +17,7 @@ def main():
 def run_test_sum_powers():
     """ Tests the   sum_powers   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_powers  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -28,6 +28,23 @@ def run_test_sum_powers():
     print('--------------------------------------------------')
     print('Testing the   sum_powers   function:')
     print('--------------------------------------------------')
+    #Test 1:
+    expected = 3.699542015
+    answer = sum_powers(4, -0.1)
+    print('Expected:', expected)
+    print('Actual:', answer)
+
+    #Test 2:
+    expected2 = 5.004790593
+    answer2 = sum_powers(5, 0.001)
+    print('Expected:', expected2)
+    print('Actual:', answer2)
+
+    #Test 3:
+    expected3 = 8.155488555
+    answer3 = sum_powers(7, 0.123)
+    print('Expected:', expected3)
+    print('Actual:', answer3)
 
 
 def sum_powers(n, p):
@@ -43,18 +60,20 @@ def sum_powers(n, p):
       -- sum_powers(100, 0.1) returns about 144.45655
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
-
-
+    total = 0
+    for k in range(1, n + 1):
+        total = total + (k ** p)
+    return total
 def run_test_sum_powers_in_range():
     """ Tests the   sum_powers_in_range   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_powers_in_range  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -65,6 +84,23 @@ def run_test_sum_powers_in_range():
     print('--------------------------------------------------')
     print('Testing the   sum_powers_in_range   function:')
     print('--------------------------------------------------')
+    #Test 1:
+    expected = 30
+    answer = sum_powers_in_range(1, 4, 2)
+    print('Expected:', expected)
+    print('Actual:', answer)
+
+    #Test 2:
+    expected2 = 25235
+    answer2 = sum_powers_in_range(4, 10, 4)
+    print('Expected:', expected2)
+    print('Actual:', answer2)
+
+    #Test 3:
+    expected3 = 17312754
+    answer3 = sum_powers_in_range(0, 4, 12)
+    print('Expected:', expected3)
+    print('Actual:', answer3)
 
 
 def sum_powers_in_range(m, n, p):
@@ -80,13 +116,16 @@ def sum_powers_in_range(m, n, p):
       -- sum_powers_in_range(3, 100, 0.1) returns about 142.384776
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers_in_range  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
-
+    total = 0
+    for k in range(m, n + 1):
+        total = total + (k ** p)
+    return total
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
